f94d97514602b40056df85f68d24ec574a557e25
Simon Lundberg <136829507+lundbergsimon@users.noreply.github.com>
2025-06-18 15:49:49 +0200

    fix: add Id field to Book class

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,6 +2,7 @@ namespace FavoriteQuoutesWebApi.Models;
 
 public class Book
 {
+    public int Id { get; set; }
     public String Title { get; set; } = string.Empty;
     public String Author { get; set; } = string.Empty;
     public DateTime? DatePublished { get; set; } = null;
