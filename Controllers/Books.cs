6e52075683fdd48a181f1b49848aac8f84d3fce1
Simon Lundberg <136829507+lundbergsimon@users.noreply.github.com>
2025-06-27 14:22:51 +0200

    WIP: update BooksController to use in-memory list for book storage

--- Controllers/Books.cs
+++ Controllers/Books.cs
@@ -5,40 +5,62 @@ using Microsoft.AspNetCore.Mvc;
 [Route("[controller]")]
 public class BooksController : ControllerBase
 {
+    // In-memory list to simulate a data store TODO: Replace with a database
+    private static List<Book> books = new List<Book>();
+    private static int nextId = 1;
+
     [HttpGet]
-    public IEnumerable<Book> GetBooks() =>
-    [
-        new Book { Id = 1, Title = "Book 1" },
-        new Book { Id = 2, Title = "Book 2" },
-        new Book { Id = 3, Title = "Book 3" }
-    ];
+    public IEnumerable<Book> GetBooks()
+    {
+        // TODO: Replace with a database
+        return books;
+    }
 
     [HttpGet("{id}")]
-    public Book GetBook(int id) {
-        return new Book { Id = 1, Title = "Book 1" };
+    public ActionResult<Book> GetBook(int id)
+    {
+        // TODO: Replace with a database
+        var book = books.FirstOrDefault(b => b.Id == id);
+        if (book == null)
+            return NotFound();
+
+        return Ok(book);
     }
 
     [HttpPost]
     public IActionResult CreateBook([FromBody] Book book)
     {
-        Console.WriteLine($"Created book: {book.Title}");
-
-        return Ok(book);
+        Console.WriteLine("Creating book: " + System.Text.Json.JsonSerializer.Serialize(book));
+        // TODO: Replace with a database
+        book.Id = nextId++;
+        books.Add(book);
+        return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
     }
 
     [HttpPut("{id}")]
-    public ActionResult<Book> EditBook(int id)
+    public IActionResult EditBook(int id, Book updatedBook)
     {
-        Console.WriteLine($"Edited book: {id}");
+        Console.WriteLine("Updating book: " + System.Text.Json.JsonSerializer.Serialize(updatedBook));
 
-        return Ok(id);
+        // TODO: Replace with a database
+        var book = books.FirstOrDefault(b => b.Id == id);
+        if (book == null)
+            return NotFound();
+
+        book.Title = updatedBook.Title;
+        book.Author = updatedBook.Author;
+
+        return NoContent();
     }
 
     [HttpDelete("{id}")]
-    public ActionResult<Book> DeleteBook(int id)
+    public IActionResult DeleteBook(int id)
     {
-        Console.WriteLine($"Edited book: {id}");
+        var book = books.FirstOrDefault(b => b.Id == id);
+        if (book == null)
+            return NotFound();
 
-        return Ok(id);
+        books.Remove(book);
+        return NoContent();
     }
 }
